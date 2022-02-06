04495570acfea80acfe4563c4ab3de365fe42da3
chiahui <ashleyhsueh33@gmail.com>
2022-02-06 21:19:22 +0800

    feat:managing state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { useState } from 'react'
+import NewTodo from './components/NewTodo'
 import Todos from './components/Todos'
 import Todo from './models/todo'
 import './App.css'
 
 function App() {
-  const todos = [new Todo('買東西'), new Todo('看書'), new Todo('寫作業')]
+  const [todos, setTodos] = useState<Todo[]>([])
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText)
+    setTodos(prevTodos => {
+      return prevTodos.concat(newTodo)
+    })
+  }
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   )
