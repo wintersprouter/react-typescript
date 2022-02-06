c8caebba2b8cf28066a6494dcb0544a72df4d6f9
chiahui <ashleyhsueh33@gmail.com>
2022-02-06 20:03:55 +0800

    feat: adding a data model

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Todos from './components/Todos'
+import Todo from './models/todo'
 import './App.css'
 
 function App() {
+  const todos = [new Todo('買東西'), new Todo('看書'), new Todo('寫作業')]
   return (
     <div>
-      <Todos items={['買東西','看書','寫作業']}/>
+      <Todos items={todos} />
     </div>
   )
 }
