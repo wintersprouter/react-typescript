7bb89610cea9ec0b6c4f7c2db5c43c71bfd5fca4
chiahui <ashleyhsueh33@gmail.com>
2022-02-06 19:51:44 +0800

    feat: add Todos component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import './App.css';
+import Todos from './components/Todos'
+import './App.css'
 
 function App() {
   return (
     <div>
+      <Todos items={['買東西','看書','寫作業']}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
